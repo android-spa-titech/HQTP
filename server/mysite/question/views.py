c2c6801616ba56cd40921c0eaec61118903c06e1
cloverrose <clover.plus.rose@gmail.com>
2012-05-24 23:53:45 +0900

    refact TEMPLATE_DIRS. use template

--- server/mysite/question/views.py
+++ server/mysite/question/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def auth_view(request):
     access_token=request.GET['access_token']
-    return HttpResponse("Welcome to HQTP ! [auth] access_token=%s" % access_token)
+    dic={'access_token':access_token}
+    return render_to_response('question/auth.html',dic)
 
 def get_view(request):
     return HttpResponse("Welcome to HQTP ! [get]") 
