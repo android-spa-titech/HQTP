1e91adbe5a79288a843c9223370e1c43f9e02016
cloverrose <clover.plus.rose@gmail.com>
2012-05-24 23:28:41 +0900

    fix htqt -> hqtp

--- server/mysite/question/views.py
+++ server/mysite/question/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 
 def auth_view(request):
     access_token=request.GET['access_token']
-    return HttpResponse("Welcome to HTQT ! [auth] access_token=%s" % access_token)
+    return HttpResponse("Welcome to HQTP ! [auth] access_token=%s" % access_token)
 
 def get_view(request):
-    return HttpResponse("Welcome to HTQT ! [get]") 
+    return HttpResponse("Welcome to HQTP ! [get]") 
 
 def post_view(request):
     title=request.POST['title']
     body=request.POST['body']
-    return HttpResponse("Welcome to HTQT ! [post] title=%s, body=%s" % (title, body))
+    return HttpResponse("Welcome to HQTP ! [post] title=%s, body=%s" % (title, body))
