abea06e64cb92606f4aaacabf1627fb00266849a
ide-an <ideanweb@gmail.com>
2012-05-29 18:40:20 +0900

    Add OnClickListener for buttons

--- client/src/org/hqtp/android/HQTPActivity.java
+++ client/src/org/hqtp/android/HQTPActivity.java
@@ -1,13 +1,54 @@
 package org.hqtp.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class HQTPActivity extends Activity {
+public class HQTPActivity extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        View verification_button = this.findViewById(R.id.verification_button);
+        verification_button.setOnClickListener(this);
+        View getallpost_button = this.findViewById(R.id.getallpost_button);
+        getallpost_button.setOnClickListener(this);
+        View post_button = this.findViewById(R.id.post_button);
+        post_button.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v) {
+    	switch (v.getId()) {
+		case R.id.verification_button:
+			showAlert("verification", "verification_button is clicked");
+			//TODO: implement
+			break;
+		case R.id.getallpost_button:
+			showAlert("getallpost", "getallpost_button is clicked");
+			//TODO: implement
+			break;
+		case R.id.post_button:
+			showAlert("post", "post_button is clicked");
+			//TODO: implement
+			break;
+		default:
+			break;
+		}
+    }
+    
+	private void showAlert(String title, String message) {
+		new AlertDialog.Builder(this)
+		.setTitle(title)
+		.setMessage(message)
+		.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+			}
+		}).show();
+	}
 }
