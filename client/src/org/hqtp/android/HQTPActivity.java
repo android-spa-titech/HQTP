89fffb0dc36fb956f279e240251c11c74de37e6e
ide-an <ideanweb@gmail.com>
2012-05-29 18:48:33 +0900

    fix indentation

--- client/src/org/hqtp/android/HQTPActivity.java
+++ client/src/org/hqtp/android/HQTPActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class HQTPActivity extends Activity implements OnClickListener{
+public class HQTPActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,35 +20,35 @@ public class HQTPActivity extends Activity implements OnClickListener{
         View post_button = this.findViewById(R.id.post_button);
         post_button.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View v) {
-    	switch (v.getId()) {
-		case R.id.verification_button:
-			showAlert("verification", "verification_button is clicked");
-			//TODO: implement
-			break;
-		case R.id.getallpost_button:
-			showAlert("getallpost", "getallpost_button is clicked");
-			//TODO: implement
-			break;
-		case R.id.post_button:
-			showAlert("post", "post_button is clicked");
-			//TODO: implement
-			break;
-		default:
-			break;
-		}
+        switch (v.getId()) {
+        case R.id.verification_button:
+            showAlert("verification", "verification_button is clicked");
+            // TODO: implement
+            break;
+        case R.id.getallpost_button:
+            showAlert("getallpost", "getallpost_button is clicked");
+            // TODO: implement
+            break;
+        case R.id.post_button:
+            showAlert("post", "post_button is clicked");
+            // TODO: implement
+            break;
+        default:
+            break;
+        }
     }
-    
-	private void showAlert(String title, String message) {
-		new AlertDialog.Builder(this)
-		.setTitle(title)
-		.setMessage(message)
-		.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				
-			}
-		}).show();
-	}
+
+    private void showAlert(String title, String message) {
+        new AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            }).show();
+    }
 }
