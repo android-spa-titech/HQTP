2f8c322737faf4765304629614c6ed7eb2fac55d
ide-an <ideanweb@gmail.com>
2012-06-02 17:53:37 +0900

    format code

--- client/src/org/hqtp/android/HQTPProxy.java
+++ client/src/org/hqtp/android/HQTPProxy.java
@@ -3,11 +3,9 @@ package org.hqtp.android;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -15,8 +13,6 @@ import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.ResponseContent;
-
 import android.net.Uri;
 import android.util.Log;
 
@@ -24,12 +20,12 @@ import android.util.Log;
 //とりあえずSingletonにしておきます
 public class HQTPProxy {
     private static HQTPProxy instance = null;
-    
-    //APIを投げる先のURL
-    //今はデバッグのためエミュレータからPC側のローカルホストへアクセスするURLを指定している
+
+    // APIを投げる先のURL
+    // 今はデバッグのためエミュレータからPC側のローカルホストへアクセスするURLを指定している
     private final String api_gateway = "http://10.0.2.2:8000/api/";
-    //private final String api_gateway = "http://www.hqtp.org/api/";
-    
+    // private final String api_gateway = "http://www.hqtp.org/api/";
+
     private CookieStore cookie_store = null;
 
     private HQTPProxy() {
@@ -38,9 +34,9 @@ public class HQTPProxy {
 
     // TODO: 引数でパラメータを受け取る。連想配列かなんか？
     // 戻り値をHttpResponseで返しているがレスポンスの文字列を返してもいいかもしれない
-    private HttpResponse sendByGet(String path) throws ClientProtocolException, IOException
-    {
-        Log.d("info",">>sendByGet");
+    private HttpResponse sendByGet(String path) throws ClientProtocolException,
+            IOException {
+        Log.d("info", ">>sendByGet");
         Uri.Builder builder = Uri.parse(api_gateway).buildUpon();
         builder.appendPath(path);
         // TODO: パラメータの指定
@@ -51,11 +47,10 @@ public class HQTPProxy {
         this.cookie_store = client.getCookieStore();
         return response;
     }
-    
+
     // TODO: パラメータの受け取り
-    private HttpResponse sendByPost(String path)
-    {
-        //TODO: implement
+    private HttpResponse sendByPost(String path) {
+        // TODO: implement
         return null;
     }
 
@@ -84,17 +79,17 @@ public class HQTPProxy {
 
     public List<Question> getQuestions() {
         // TODO:implement
-//        return null;
-        Log.d("info",">>getQuestions");
-//        ネットワークからの読込テスト
+        // return null;
+        Log.d("info", ">>getQuestions");
+        // ネットワークからの読込テスト
         try {
             HttpResponse res = sendByGet("");
             Log.d("info", getResponseContentText(res));
         } catch (Exception e1) {
-            Log.d("err",e1.getMessage());
+            Log.d("err", e1.getMessage());
             e1.printStackTrace();
         }
-//テスト
+        // テスト
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -102,23 +97,24 @@ public class HQTPProxy {
             e.printStackTrace();
         }
         ArrayList<Question> res = new ArrayList<Question>();
-        for(int i=0;i<10;i++){
-            res.add(new Question("質問"+new Integer(i).toString(), "質問です", "author"));
+        for (int i = 0; i < 10; i++) {
+            res.add(new Question("質問" + new Integer(i).toString(), "質問です",
+                    "author"));
         }
         return res;
     }
-    
-    private static String getResponseContentText(HttpResponse response) throws IllegalStateException, IOException
-    {
+
+    private static String getResponseContentText(HttpResponse response)
+            throws IllegalStateException, IOException {
         HttpEntity entity = response.getEntity();
-        if(entity==null){
+        if (entity == null) {
             return null;
         }
-        BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                entity.getContent()));
         StringBuilder sb = new StringBuilder();
         String line;
-        while((line = br.readLine())!=null)
-        {
+        while ((line = br.readLine()) != null) {
             sb.append(line).append("\n");
         }
         return sb.toString();
