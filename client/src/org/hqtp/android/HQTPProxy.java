24bc73abf3ba3d37621411b840688cd35cfc83f4
ide-an <ideanweb@gmail.com>
2012-06-02 20:17:54 +0900

    cookieまわりのデバッグ表示
    cookieの受け渡しについて正常動作することを確認した。

--- client/src/org/hqtp/android/HQTPProxy.java
+++ client/src/org/hqtp/android/HQTPProxy.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -11,6 +12,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import android.net.Uri;
@@ -45,6 +47,13 @@ public class HQTPProxy {
         client.setCookieStore(cookie_store);
         HttpResponse response = client.execute(http_get);
         this.cookie_store = client.getCookieStore();
+        {//Cookieのデバッグ表示
+            Iterator<Cookie> c_it = cookie_store.getCookies().iterator();
+            while (c_it.hasNext()) {
+                Cookie c = c_it.next();
+                Log.d("cookie", c.getName() + ":" + c.getValue());
+            }
+        }
         return response;
     }
 
