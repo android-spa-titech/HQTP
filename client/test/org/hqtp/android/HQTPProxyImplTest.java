b6b7884e52bc9b663628a63986fafeeef70ecdf8
ide-an <ideanweb@gmail.com>
2012-06-05 21:52:07 +0900

    getQuestions()についてテストを記述 現状ではJSONまわりのテストがうまくいかない どうやらrobolectricの問題？   https://groups.google.com/group/robolectric/browse_thread/thread/4d2c4ca6eba5fa86

--- client/test/org/hqtp/android/HQTPProxyImplTest.java
+++ client/test/org/hqtp/android/HQTPProxyImplTest.java
@@ -4,8 +4,10 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 import java.net.URI;
+import java.util.List;
 
 import org.apache.http.client.methods.HttpUriRequest;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -50,6 +52,24 @@ public class HQTPProxyImplTest {
         assertThat(sentHttpRequest.getMethod(), equalTo("POST"));
         //TODO: パラメータについても検査したい
         //Assert.assertTrue((EntityUtils.toString(((HttpPost)sentHttpRequest).getEntity())), false);
+    }
+
+    @Test
+    public void getQuestionsShouldCallAPI() throws Exception {
+        Robolectric.clearHttpResponseRules();
+        Robolectric.clearPendingHttpResponses();
+        Robolectric.addPendingHttpResponse(200, "{\"status\":\"OK\",\"posts\":[{\"title\":\"title1\",\"body\":\"body1\"},{\"title\":\"title2\",\"body\":\"body2\"}]}");
+
+        List<Question> res = proxy.getQuestions();
+
+        HttpUriRequest sentHttpRequest = (HttpUriRequest) Robolectric.getSentHttpRequest(0);
+        assertThat(sentHttpRequest.getURI(), equalTo(URI.create("http://www.hqtp.org/api/get/")));
+        Assert.assertNotNull(res);
+        Assert.assertEquals(2, res.size());
+        assertThat(res.get(0).getTitle(), equalTo("title1"));
+        assertThat(res.get(0).getBody(), equalTo("body1"));
+        assertThat(res.get(1).getTitle(), equalTo("title2"));
+        assertThat(res.get(1).getBody(), equalTo("body2"));
     }
 
 }
