57f38891ad0fa2f5cb56f0db77b6716c019ee5f0
cloverrose <clover.plus.rose@gmail.com>
2012-06-08 06:03:43 +0900

    refact json_response

--- server/mysite/question/views.py
+++ server/mysite/question/views.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseForbidden
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -13,24 +13,27 @@ def convert_context_to_json(context):
     # -- can be serialized as JSON.
     return json.dumps(context)
 
-def json_response(context):
+def json_response(context={}):
+    context['status']='OK'
     return HttpResponse(convert_context_to_json(context),mimetype='application/json')
-def json_response_not_found(context):
+def json_response_not_found(context={}):
+    context['status']='Not Found'
     return HttpResponseNotFound(convert_context_to_json(context),mimetype='application/json')
+def json_response_forbidden(context={}):
+    context['status']='Forbidden'
+    return HttpResponseForbidden(convert_context_to_json(context),mimetype='application/json')
 
 def auth_view(request):
     access_token=request.GET['access_token']
     # TODO auth
 
-    context={'status':'OK'}
-    return json_response(context)
+    return json_response()
 
 def get_view(request):
     # TODO load questions from database
 
     context=dict(
-        status='ok'
-       ,posts=[
+       posts=[
             dict(title=u'課題',body=u'今日の課題の範囲が分からない')
            ,dict(title='NFA?',body=u'NFAってなんの略だっけ？')
            ,dict(title=u'状態数orz',body=u'決定化してるんだけど、状態めちゃ増える。人間にやらせるなんて')
@@ -44,5 +47,4 @@ def post_view(request):
     body=request.POST['body']
     # TODO save this post
 
-    context={'status':'OK'}
-    return json_response(context)
+    return json_response()
