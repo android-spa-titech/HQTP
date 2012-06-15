e62a2b5f60bfe29491dbb7ac2b03aa99bc7501f5
Naoki Kabasawa <kabasawa.n.aa@m.titech.ac.jp>
2012-06-15 15:47:20 +0900

    QuestionAdapterを実装

--- client/src/org/hqtp/android/ListQuestionActivity.java
+++ client/src/org/hqtp/android/ListQuestionActivity.java
@@ -1,5 +1,6 @@
 package org.hqtp.android;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.inject.Inject;
@@ -11,29 +12,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 import roboguice.util.RoboAsyncTask;
 
 public class ListQuestionActivity extends RoboActivity {
 
-    // グリッド読み込み
-    @Inject HQTPProxy proxy;
-        
+    @InjectView(R.id.listQuestion)
+    ListView questionListView;
+    
+    @InjectView(R.id.question_title)
+    EditText titleEditText;
+    @InjectView(R.id.question_body)
+    EditText bodyEditText;
+    @Inject
+    HQTPProxy proxy;
+
+    static List<Question> questionList = new ArrayList<Question>();
+    static QuestionAdapter adapter;
+
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list);
         
+        adapter = new QuestionAdapter( this, R.layout.list, questionList);
+        questionListView.setAdapter(adapter);
+
+        // クリックされたとき、選択されたときのコールバックリスナーを登録
+
         GetQuestion gq = new GetQuestion();
         try {
             gq.execute();
         } catch (Exception e){
             e.printStackTrace();
         }
-        // 読み込みキャンセルしたい場合は何かしないといけない
+        // 読み込みをキャンセルしたい場合は何かしないといけない
+
     }
+    
+
  
     private class GetQuestion extends RoboAsyncTask<List<Question>> {
         @Override
@@ -52,6 +75,7 @@ public class ListQuestionActivity extends RoboActivity {
                     sb.append("\n");
                     sb.append(q.getBody());
                     sb.append("\n");
+                    questionList.add( q );
                 }
             }
             Log.d("info", sb.toString());       
@@ -60,13 +84,16 @@ public class ListQuestionActivity extends RoboActivity {
         // onExceptionとかしたほうがいいのか
     }
     
-    
+
+   // BaseAdapterを継承するのがよい？
    private class QuestionAdapter extends ArrayAdapter<Question> {
 
         private int resourceId;
+        private LayoutInflater inflater;
 
         public QuestionAdapter(Context context, int resource, List<Question> questions) {
             super(context, resource, questions);
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE); // ここ
             resourceId = resource;
         }
 
@@ -75,19 +102,20 @@ public class ListQuestionActivity extends RoboActivity {
 
             Question question = (Question)getItem(position);
             
+            // メモリを無駄に使わないようにらしい
             if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE); // ここ
                 convertView = inflater.inflate(resourceId, null);
             }
 
-            TextView titleview = (TextView)convertView.findViewById(R.id.a); // ここ
-            TextView bodyview  = (TextView)conveetView.findViewById(R.id.b); // ここ
+            TextView titleview = (TextView)convertView.titleEditText;
+            TextView bodyview  = (TextView)convertView.bodyEditText;
             titleview.setText( question.getTitle() );
             bodyview.setText( question.getBody() );
             
-            return view;
+            // ボタンを付けるときはここでSetOnClickListenerを設定
+            
+            return convertView;
         }
 
     }
-    
 }
