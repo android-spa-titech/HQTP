2bdf4512c99a1e29412a61b12baef4760e563b8c
draftcode <draftcode@gmail.com>
2012-06-17 23:58:37 +0900

    Change the structure of the authorization.
    * Unify AuthorizationCallBackActivity and LoginActivity.
    * The LoginActivity has the 'singleTask' launchMode. This means the activity
      has at most one activity in one system. As a consequence, the activity  can
      hold requestToken while authentication process. The reason why we don't use
      'singleTop' activity is the browser will not call any existing activity
      instance in the callback process. Instead, it will create the new activity.
      And the reason why we don't use 'singleInstance' is it cannot receive any
      intent with any extras.

--- client/src/org/hqtp/android/LoginActivity.java
+++ client/src/org/hqtp/android/LoginActivity.java
@@ -1,48 +1,99 @@
 package org.hqtp.android;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import roboguice.activity.RoboActivity;
-import roboguice.util.RoboAsyncTask;
+import roboguice.util.SafeAsyncTask;
+import twitter4j.auth.AccessToken;
 import twitter4j.auth.OAuthAuthorization;
 import twitter4j.auth.RequestToken;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationContext;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.inject.Inject;
 
 public class LoginActivity extends RoboActivity {
-    //TODO: リクエストトークンの渡し方の改善
-    //AuthorizationCallBackActivityにリクエストトークンを渡すために
-    //request_tokenをstaticに用意している。
-    //あまりいい方法とは思えないので何かいい案があれば変えて欲しい。
-    private static RequestToken request_token;
-    public static RequestToken getRequestToken() {
-        return request_token;
-    }
-    
+
+    private final String callback_url = "hqtp://request_callback/";
+    private OAuthAuthorization oauth;
+    private RequestToken requestToken;
+
+    @Inject
+    private HQTPProxy proxy;
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        Configuration config = ConfigurationContext.getInstance();
+        oauth = new OAuthAuthorization(config);
+        oauth.setOAuthConsumer(getString(R.string.consumer_key), getString(R.string.consumer_secret));
+
+        if (savedInstanceState != null && savedInstanceState.containsKey("REQUEST_TOKEN")) {
+            requestToken = (RequestToken) savedInstanceState.getSerializable("REQUEST_TOKEN");
+        }
     }
 
-    private final String callback_url = "hqtp://request_callback/";
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Uri uri = intent.getData();
+        if (uri != null && uri.getScheme().equals("hqtp")) {
+            AfterTwitterAuthorizationTask task = new AfterTwitterAuthorizationTask(uri);
+            getInjector().injectMembers(task);
+            task.execute();
+        }
+    }
 
-    //TODO: onclickで呼ぶ
-    private class TwitterAuthorizationTask extends RoboAsyncTask<Void>
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("REQUEST_TOKEN", requestToken);
+    }
+
+    private class AfterTwitterAuthorizationTask extends SafeAsyncTask<Void>
+    {
+        Uri uri;
+
+        AfterTwitterAuthorizationTask(Uri uri) {
+            super();
+            this.uri = uri;
+        }
+
+        @Override
+        public Void call() throws Exception {
+            AccessToken accessToken = oauth.getOAuthAccessToken(requestToken, uri.getQueryParameter("oauth_verifier"));
+            proxy.authenticate(accessToken.getToken(), accessToken.getTokenSecret());
+            return null;
+        }
+
+        @Override
+        protected void onException(Exception e) {
+            // TODO: ここで上手く行かなかった場合の通知を行う。
+        }
+
+        @Override
+        protected void onSuccess(Void v) {
+            startActivity(new Intent(LoginActivity.this, HQTPActivity.class));
+        }
+    }
+
+    // TODO: onClickで呼ぶ
+    private class TwitterAuthorizationTask extends SafeAsyncTask<Void>
     {
         @Override
         public Void call() throws Exception {
-            Configuration config = ConfigurationContext.getInstance();
-            OAuthAuthorization oauth = new OAuthAuthorization(config);
-            oauth.setOAuthConsumer(getString(R.string.consumer_key), getString(R.string.consumer_secret));
-            try {
-                request_token = oauth.getOAuthRequestToken(callback_url);
-                String uri = request_token.getAuthorizationURL();
-                startActivityForResult(new Intent(Intent.ACTION_VIEW, Uri.parse(uri)), 0);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            requestToken = oauth.getOAuthRequestToken(callback_url);
+            String uri = requestToken.getAuthorizationURL();
+            startActivity((new Intent(Intent.ACTION_VIEW, Uri.parse(uri))));
             return null;
+        }
+
+        @Override
+        protected void onException(Exception e) {
+            // TODO: ここで上手く行かなかった場合の通知を行う。
         }
     }
 }
