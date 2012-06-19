d9b6b994d19e20c1ff45a9e5129b2ed1b2be252c
cloverrose <clover.plus.rose@gmail.com>
2012-06-19 13:15:55 +0900

    customize admin to show screen_name and name at admin-site

--- server/mysite/question/admin.py
+++ server/mysite/question/admin.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
 
 from django.contrib import admin
-from mysite.question.models import Question
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from mysite.question.models import Question, UserProfile
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -9,6 +11,18 @@ class QuestionAdmin(admin.ModelAdmin):
     date_hierarchy = 'posted_at'
 
 admin.site.register(Question, QuestionAdmin)
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    extra = 0
+
+
+class MyUserAdmin(UserAdmin):
+    inlines = UserAdmin.inlines + [UserProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
 
 
 def dummytest():
