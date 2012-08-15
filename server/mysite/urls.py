0d11c0f9b8d145bd946e64a428675890f31d0b6a
cloverrose <clover.plus.rose@gmail.com>
2012-08-15 20:25:43 +0900

    update url dispatch

--- server/mysite/urls.py
+++ server/mysite/urls.py
@@ -10,8 +10,14 @@ urlpatterns = patterns('',
     # url(r'^mysite/', include('mysite.foo.urls')),
 
     url(r'^api/auth/?$', 'mysite.question.views.auth_view'),
-    url(r'^api/get/?$', 'mysite.question.views.get_view'),
-    url(r'^api/post/?$', 'mysite.question.views.post_view'),
+    # url(r'^api/get/?$', 'mysite.question.views.get_view'),
+    # url(r'^api/post/?$', 'mysite.question.views.post_view'),
+    url(r'^api/lecture/get/?$',
+        'mysite.question.views.lecture_get_view'),
+    url(r'^api/lecture/add/?$',
+        'mysite.question.views.lecture_add_view'),
+    url(r'^api/lecture/timeline/?$',
+        'mysite.question.views.lecture_timeline_view'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
