2e8b07eab7f4720caa9051ca1004c5fbbc5705c3
nkbsw <kabasawa.n.aa@m.titech.ac.jp>
2012-08-15 20:49:14 +0900

    create files

---
+++ client/src/org/hqtp/android/AddLectureActivity.java
@@ -0,0 +1,15 @@
+package org.hqtp.android;
+
+import roboguice.activity.RoboActivity;
+import android.os.Bundle;
+
+public class AddLectureActivity extends RoboActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_lecture);
+    }
+
+
+}
