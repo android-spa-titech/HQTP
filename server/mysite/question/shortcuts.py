df445bb2168a3c633fde22f6d2b7d8e6a6e1ea89
cloverrose <clover.plus.rose@gmail.com>
2012-09-01 21:16:28 +0900

    update test without target code

--- server/mysite/question/shortcuts.py
+++ server/mysite/question/shortcuts.py
@@ -3,12 +3,10 @@
 from mysite.question.twutil.consumer_info import spa_key, spa_secret
 from django.test.client import Client
 import json
-import datetime
 
 
 def make_client():
     """
-    >>> from mysite.question.shortcuts import make_client
     >>> c = make_client()
     """
 
@@ -18,19 +16,20 @@ def make_client():
 
 def access_auth_view(client, key=None, secret=None):
     """
-    >>> from mysite.question.tests import clean_users
-    >>> clean_users()
-
-    >>> from mysite.question.shortcuts import (make_client,
-    ...                                        access_auth_view)
     >>> c = make_client()
     >>> jobj = access_auth_view(c)
     >>> jobj['status'] == 'OK'
     True
-    >>> jobj['created']
+    >>> 'created' in jobj
+    True
+    >>> user = jobj['user']
+    >>> 'id' in user
+    True
+    >>> 'name' in user
+    True
+    >>> 'icon_url' in user
     True
     """
-
     # for convinient, key and secret are allowed blank
     # if blank then use android_spa's key and secret
     if key is None and secret is None:
@@ -46,68 +45,123 @@ def access_auth_view(client, key=None, secret=None):
     return jobj
 
 
-def access_get_view(client):
+def access_lecture_get_view(client):
     """
-    This test is old.
-    /api/getは使われていない
-    """
-    """
-    >>> from mysite.question.tests import clean_questions
-    >>> clean_questions()
-
-    >>> from mysite.question.shortcuts import (make_client,
-    ...                                        access_auth_view,
-    ...                                        access_get_view)
     >>> c = make_client()
     >>> jobj = access_auth_view(c)
-    >>> jobj = access_get_view(c)
+    >>> jobj = access_lecture_get_view(c)
     >>> jobj['status'] == 'OK'
     True
-    >>> jobj['posts'] == []
+    >>> 'lectures' in jobj
     True
     """
-
-    url = '/api/get/'
+    url = '/api/lecture/get/'
     response = client.get(url)
     jojb = json.loads(response.content)
     return jojb
 
 
-def access_post_view(client, title=None, body=None):
+def access_lecture_add_view(client, name, code):
     """
-    This test is old.
-    /api/postは使われていない
-    """
-    """
-    >>> from mysite.question.tests import clean_questions
-    >>> clean_questions()
-
-    >>> from mysite.question.shortcuts import (make_client,
-    ...                                        access_auth_view,
-    ...                                        access_get_view,
-    ...                                        access_post_view)
+    >>> name = 'Programming 1'
+    >>> code = '0B123456789'
     >>> c = make_client()
     >>> jobj = access_auth_view(c)
-    >>> jobj = access_post_view(c, 'Hello', 'World')
+    >>> jobj = access_lecture_add_view(c, name=name, code=code)
     >>> jobj['status'] == 'OK'
     True
-    >>> jobj = access_get_view(c)
-    >>> post = jobj['posts'][0]
-    >>> post['title'] == 'Hello'
+    >>> 'created' in jobj
     True
-    >>> post['body'] == 'World'
+
+    >>> lecture = jobj['lecture']
+    >>> lecture['name'] == name
+    True
+    >>> lecture['code'] == code
+    True
+    >>> 'id' in lecture
     True
     """
+    url = '/api/lecture/add/'
+    response = client.post(url, dict(name=name, code=code))
+    jobj = json.loads(response.content)
+    return jobj
 
-    # for convenient, title and body are allowed blank
-    # to make unique title and body, use now date and time
-    now = str(datetime.datetime.now())
-    if title is None:
-        title = 'TITLE:' + now
-    if body is None:
-        body = 'BODY:' + now
 
-    url = '/api/post/'
-    response = client.post(url, dict(title=title, body=body))
+def access_timeline_get_view(client, id):
+    u"""
+    >>> name = 'Arch1'
+    >>> code = 't001'
+
+    # 下準備（授業の作成）
+    >>> c0 = make_client()
+    >>> jobj0a = access_auth_view(c0)
+    >>> jobj0b = access_lecture_add_view(c, name=name, code=code)
+    >>> lecture_id = jobj0b['lecture']['id']
+
+    >>> c = make_client()
+    >>> jobj = access_auth_view(c)
+    >>> jobj = access_timeline_get_view(c, id=lecture_id)
+    >>> jobj['status'] == 'OK'
+    True
+    >>> 'posts' in jojb
+    True
+    """
+    url = '/api/lecture/timeline/'
+    response = client.get(url)
+    jojb = json.loads(response.content)
+    return jojb
+
+
+def access_timeline_post_view(client, id, body,
+                              before_virtual_ts, after_virtual_ts):
+    u"""
+    >>> name = 'Arch1'
+    >>> code = 't001'
+    >>> body = u'MIPSとは'
+
+    # 下準備（授業の作成）
+    >>> c0 = make_client()
+    >>> jobj0a = access_auth_view(c0)
+    >>> jobj0b = access_lecture_add_view(c, name=name, code=code)
+    >>> lecture_id = jobj0b['lecture']['id']
+
+    >>> c = make_client()
+    >>> jobj = access_auth_view(c)
+    >>> jobj = access_timeline_post_view(c, id=lecture_id, body=body,
+    ...                                  before_virtual_ts=1000,
+    ...                                  after_virtual_ts=2000)
+    >>> jobj['status'] == 'OK'
+    True
+    >>> post = jojb['post']
+    >>> 'id' in post
+    True
+
+    # 追加先授業の情報は正しいか
+    >>> lecture = post['lecture']
+    >>> lecture['id'] == lecture_id
+    True
+    >>> lecture['name'] == name
+    True
+    >>> lecture['code'] == code
+    True
+
+    # 投稿内容の情報は正しいか
+    >>> post['body'] == body
+    True
+
+    # 投稿ユーザーの情報は正しいか
+    >>> user = post['user']
+    >>> 'id' in user and 'name' in user and 'icon_url' in user
+    True
+
+    >>> 'time' in post
+    True
+    >>> 'virtual_ts' in post
+    True
+    """
+    url = '/api/lecture/timeline/'
+    response = client.post(url, dict(id=id, body=body,
+                                     before_virtual_ts=before_virtual_ts,
+                                     after_virtual_ts=after_virtual_ts))
     jobj = json.loads(response.content)
     return jobj
