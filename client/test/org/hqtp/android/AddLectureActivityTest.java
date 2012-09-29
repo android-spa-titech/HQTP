ef4f16773e3c67b62f52f30c68223a129cbcfd15
draftcode <draftcode@gmail.com>
2012-09-29 21:44:20 +0900

    Make some tests simple.

--- client/test/org/hqtp/android/AddLectureActivityTest.java
+++ client/test/org/hqtp/android/AddLectureActivityTest.java
@@ -1,10 +1,7 @@
 package org.hqtp.android;
 
 import static com.xtremelabs.robolectric.Robolectric.shadowOf;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -13,47 +10,36 @@ import static org.mockito.Mockito.when;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import roboguice.inject.InjectView;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.shadows.ShadowActivity;
 import com.xtremelabs.robolectric.shadows.ShadowAlertDialog;
-import com.xtremelabs.robolectric.shadows.ShadowIntent;
 
 @RunWith(AddLectureActivityTestRunner.class)
 public class AddLectureActivityTest {
 
     @Inject
-    Injector injector;
-    @Inject
     HQTPProxy proxy;
-
-    @Test
-    public void activityShouldHaveComponents() {
-        AddLectureActivity activity = new AddLectureActivity();
-        injector.injectMembers(activity);
-        activity.onCreate(null);
-
-        assertNotNull(activity.findViewById(R.id.lecture_code_text));
-        assertNotNull(activity.findViewById(R.id.lecture_name_text));
-        assertNotNull(activity.findViewById(R.id.lecture_add_button));
-        assertNotNull(activity.findViewById(R.id.lecture_add_cancel_button));
-    }
+    @Inject
+    AddLectureActivity activity;
+    @InjectView(R.id.lecture_code_text)
+    TextView lectureCodeText;
+    @InjectView(R.id.lecture_name_text)
+    TextView lectureNameText;
+    @InjectView(R.id.lecture_add_button)
+    Button addButton;
+    @InjectView(R.id.lecture_add_cancel_button)
+    Button cancelButton;
 
     @Test
     public void pressingTheCancelButtonShouldNotCallProxy() throws Exception {
-        AddLectureActivity activity = new AddLectureActivity();
-        injector.injectMembers(activity);
         activity.onCreate(null);
-
-        TextView lectureCodeText = (TextView) activity.findViewById(R.id.lecture_code_text);
-        TextView lectureNameText = (TextView) activity.findViewById(R.id.lecture_name_text);
-        Button cancelButton = (Button) activity.findViewById(R.id.lecture_add_cancel_button);
 
         lectureCodeText.setText("lectureCode");
         lectureNameText.setText("lectureName");
@@ -69,12 +55,7 @@ public class AddLectureActivityTest {
 
     @Test
     public void pressingTheBackButtonShouldNotCallProxy() throws Exception {
-        AddLectureActivity activity = new AddLectureActivity();
-        injector.injectMembers(activity);
         activity.onCreate(null);
-
-        TextView lectureCodeText = (TextView) activity.findViewById(R.id.lecture_code_text);
-        TextView lectureNameText = (TextView) activity.findViewById(R.id.lecture_name_text);
 
         lectureCodeText.setText("lectureCode");
         lectureNameText.setText("lectureName");
@@ -90,13 +71,7 @@ public class AddLectureActivityTest {
 
     @Test
     public void pressingTheAddButtonShouldCallProxy() throws Exception {
-        AddLectureActivity activity = new AddLectureActivity();
-        injector.injectMembers(activity);
         activity.onCreate(null);
-
-        TextView lectureCodeText = (TextView) activity.findViewById(R.id.lecture_code_text);
-        TextView lectureNameText = (TextView) activity.findViewById(R.id.lecture_name_text);
-        Button addButton = (Button) activity.findViewById(R.id.lecture_add_button);
 
         lectureCodeText.setText("lectureCode");
         lectureNameText.setText("lectureName");
@@ -107,7 +82,7 @@ public class AddLectureActivityTest {
         verify(proxy).addLecture("lectureCode", "lectureName");
         ShadowActivity shadowActivity = shadowOf(activity);
         Intent startedIntent = shadowActivity.getNextStartedActivity();
-        // TODO: Uncomment if HQTPProxyImple.addLecture() is implemented.
+        // TODO: Uncomment when HQTPProxyImple.addLecture() is implemented.
         // assertNotNull(startedIntent);
         // ShadowIntent shadowIntent = Robolectric.shadowOf(startedIntent);
         // assertThat(shadowIntent.getComponent().getClassName(),
@@ -116,13 +91,7 @@ public class AddLectureActivityTest {
 
     @Test
     public void failingPostShouldShowAlert() throws Exception {
-        AddLectureActivity activity = new AddLectureActivity();
-        injector.injectMembers(activity);
         activity.onCreate(null);
-
-        TextView lectureCodeText = (TextView) activity.findViewById(R.id.lecture_code_text);
-        TextView lectureNameText = (TextView) activity.findViewById(R.id.lecture_name_text);
-        Button addButton = (Button) activity.findViewById(R.id.lecture_add_button);
 
         when(proxy.addLecture("lectureCode", "lectureName")).thenThrow(
                 new HQTPAPIException("Cannot post"));
@@ -134,7 +103,7 @@ public class AddLectureActivityTest {
 
         verify(proxy).addLecture("lectureCode", "lectureName");
         AlertDialog alert = ShadowAlertDialog.getLatestAlertDialog();
-        // TODO: Uncomment if error handling code is implemented.
+        // TODO: Uncomment when error handling code is implemented.
         // assertNotNull(alert);
     }
 
