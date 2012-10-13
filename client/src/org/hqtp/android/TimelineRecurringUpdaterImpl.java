eedfd11466366ceca0361e5471a8e930db1f92d5
draftcode <draftcode@gmail.com>
2012-10-13 21:08:08 +0900

    Change implementation of TimelineRecurringUpdaterImpl.
    TimelineRecurringUpdater need to be ready for restart. This is requried
    because activities occasionally pause. This forces to change the way to
    implement updater. Just canceling the task is not enough.
    
    Moreover, executor's cancellation is difficult in generalto use because it is
    difficult to find the right way to cancel on-going processing and to clean up
    everything at the arbitrary time. Because of this, this commit lessen the
    direct use of executor framework.

--- client/src/org/hqtp/android/TimelineRecurringUpdaterImpl.java
+++ client/src/org/hqtp/android/TimelineRecurringUpdaterImpl.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import roboguice.inject.ContextSingleton;
+import roboguice.util.SafeAsyncTask;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
@@ -22,13 +23,8 @@ public class TimelineRecurringUpdaterImpl implements TimelineRecurringUpdater {
     Long timeline_update_period;
 
     private int lecture_id;
-    private ScheduledFuture<?> scheduled_future;
+    private RecurringTaskManager manager;
     private Set<TimelineObserver> observers = new HashSet<TimelineObserver>();
-
-    @Override
-    public void stop() {
-        scheduled_future.cancel(true);
-    }
 
     @Override
     public void registerTimelineObserver(TimelineObserver observer) {
@@ -41,33 +37,77 @@ public class TimelineRecurringUpdaterImpl implements TimelineRecurringUpdater {
     }
 
     @Override
-    public void startRecurringUpdateTimeline() {
-        ScheduledExecutorService timeline_thread = Executors.newSingleThreadScheduledExecutor();
-        scheduled_future = timeline_thread.scheduleWithFixedDelay(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    if (lecture_id != -1) {
-                        List<Post> posts = proxy.getTimeline(lecture_id);
-                        if (!scheduled_future.isCancelled()) {
-                            notify_update(posts);
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }, 0, timeline_update_period, TimeUnit.MILLISECONDS);
+    public void setLectureId(int lecture_id) {
+        this.lecture_id = lecture_id;
     }
 
-    private void notify_update(List<Post> posts) {
-        for (TimelineObserver observer : observers) {
-            observer.onUpdate(posts);
+    @Override
+    public synchronized void startRecurringUpdateTimeline() {
+        if (manager == null) {
+            manager = new RecurringTaskManager();
+            manager.start();
         }
     }
 
     @Override
-    public void setLectureId(int lecture_id) {
-        this.lecture_id = lecture_id;
+    public synchronized void stop() {
+        if (manager != null) {
+            manager.stop();
+            manager = null;
+        }
+    }
+
+    // RecurringTaskManager is used to avoid unexpected results related to multi-threading.
+    // This class is happened to be restart. If you use Executor framework's
+    // recurring execution, you might call cancel method in the stop method.
+    // But it is difficult to handle InterruptedException with you notifying
+    // results to observer in UI thread. This class will use SafeAsyncTask
+    // to avoid this complicated exception handling.
+    private class RecurringTaskManager {
+        private ScheduledExecutorService wait_thread = Executors.newSingleThreadScheduledExecutor();
+        private AtomicBoolean stopped = new AtomicBoolean(false);
+        private RecurringTask task;
+
+        public void start() {
+            task = new RecurringTask();
+            task.execute();
+        }
+
+        public void stop() {
+            stopped.set(true);
+            wait_thread.shutdown();
+        }
+
+        private class RecurringTask extends SafeAsyncTask<List<Post>> {
+            @Override
+            public List<Post> call() throws Exception {
+                return proxy.getTimeline(lecture_id);
+            }
+
+            @Override
+            protected void onSuccess(List<Post> posts) throws Exception {
+                if (!stopped.get()) {
+                    for (TimelineObserver observer : observers) {
+                        observer.onUpdate(posts);
+                    }
+                }
+            }
+
+            @Override
+            protected void onFinally() {
+                if (!stopped.get()) {
+                    wait_thread.schedule(new Runnable() {
+                        @Override
+                        public void run() {
+                            // It is probable that this task is stopped during delayed time.
+                            if (!stopped.get()) {
+                                task = new RecurringTask();
+                                task.execute();
+                            }
+                        }
+                    }, timeline_update_period, TimeUnit.MILLISECONDS);
+                }
+            }
+        }
     }
 }
