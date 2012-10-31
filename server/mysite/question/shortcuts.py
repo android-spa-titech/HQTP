cf9b1b588fb93956b08301727095c9c64fb051b6
strviola <SuzukiRyota@strviola.local>
2012-10-31 13:34:19 +0900

    Revert: Refactoring shortcuts.py

--- server/mysite/question/shortcuts.py
+++ server/mysite/question/shortcuts.py
@@ -6,46 +6,61 @@ import json
 
 
 def make_client():
-    """>>> c = make_client()"""
-    return Client()
-
-
-def access_template(method, api_name, **kwargs):
-    u"""
-    他のショートカット関数を生成するためのひな形
-    Bad Requestについてテストする時以外は、これを直接呼び出すようなことはしない
     """
-    url = '/api/' + api_name + '/'
-    response = method(url, kwargs)
+    >>> c = make_client()
+    """
+
+    return Client(enforce_csrf_checks=True)
+
+
+def access_auth_view(client, key=None, secret=None):
+    # どちらもNoneかどちらもnot Noneでなければいけない
+    error_msg = 'Usage: access_auth_view({KEY}, {SECRET})'
+    assert not((key is None) != (secret is None)), error_msg
+
+    # for convenient, key and secret are allowed blank
+    # if blank then use android_spa's key and secret
+    if key is None and secret is None:
+        key = spa_key
+        secret = spa_secret
+
+    url_template = '/api/auth/?access_token_key=%s&access_token_secret=%s'
+    url = url_template % (key, secret)
+    response = client.get(url)
     return json.loads(response.content)
 
 
-def access_auth_view(client, **key_and_secret):
-    if key_and_secret == {}:
-        # for convenient, key and secret are allowed blank
-        # if blank then use android_spa's key and secret
-        return access_template(client.get, 'auth', access_token_key=spa_key,
-                               access_token_secret=spa_secret)
-    else:
-        return access_template(client.get, 'auth', **key_and_secret)
-
-
 def access_lecture_get_view(client):
-    return access_template(client.get, 'lecture/get')
+    url = '/api/lecture/get/'
+    response = client.get(url)
+    return json.loads(response.content)
 
 
 def access_lecture_add_view(client, name, code):
-    return access_template(client.post, 'lecture/add', name=name, code=code)
+    url = '/api/lecture/add/'
+    response = client.post(url, dict(name=name, code=code))
+    return json.loads(response.content)
 
 
 def access_timeline_get_view(client, lecture_id):
-    return access_template(client.get, 'lecture/timeline', id=lecture_id)
+    url = '/api/lecture/timeline/?id=%s'
+    response = client.get(url % lecture_id)
+    return json.loads(response.content)
 
 
-def access_timeline_post_view(client, lecture_id, body, **vts):
-    return access_template(client.post, 'lecture/timeline',
-                           id=lecture_id, body=body, **vts)
+def access_timeline_post_view(client, lecture_id, body,
+                              before_virtual_ts=None, after_virtual_ts=None):
+    url = '/api/lecture/timeline/'
+    dic = dict(id=lecture_id, body=body)
+    if before_virtual_ts is not None:
+        dic['before_virtual_ts'] = before_virtual_ts
+    if after_virtual_ts is not None:
+        dic['after_virtual_ts'] = after_virtual_ts
+    response = client.post(url, dic)
+    return json.loads(response.content)
 
 
 def access_user_get_view(client, user_id):
-    return access_template(client.get, 'user', id=user_id)
+    url = '/api/user/?id=%d'
+    response = client.get(url % user_id)
+    return json.loads(response.content)
