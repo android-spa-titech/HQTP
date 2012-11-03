aaf0ed3a026ee507d4ab0681224a967ab49c9106
draftcode <draftcode@gmail.com>
2012-11-03 17:07:38 +0900

    Implement timeline delta get API

--- client/src/org/hqtp/android/HQTPProxy.java
+++ client/src/org/hqtp/android/HQTPProxy.java
@@ -17,6 +17,9 @@ public interface HQTPProxy {
     public abstract List<Post> getTimeline(int lectureId) throws IOException, HQTPAPIException, JSONException,
             ParseException;
 
+    public abstract List<Post> getTimeline(int lectureId, int sinceId) throws IOException, HQTPAPIException, JSONException,
+            ParseException;
+
     /**
      * @param body 送信する投稿本文
      * @param lectureId 授業ID
